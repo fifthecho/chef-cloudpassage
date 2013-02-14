cb23dc40349505db677b14b3ba372ff116316eb9
Jeff Moody <fifthecho@gmail.com>
2013-02-14 11:29:14 -0500

    Remove sudo

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dev@escapestudios.com, jmoody@datapipe.com"
 license          "MIT"
 description      "Installs/Configures cloudpassage"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 
 supports "ubuntu"
 supports "debian"
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,10 +9,10 @@
 case node[:platform]
 	when "debian", "ubuntu"
 		#apt
-		command = "echo 'deb http://packages.cloudpassage.com/#{node[:cloudpassage][:repository_key]}/debian debian main' | sudo tee /etc/apt/sources.list.d/cloudpassage.list > /dev/null"
+		command = "echo 'deb http://packages.cloudpassage.com/#{node[:cloudpassage][:repository_key]}/debian debian main' | tee /etc/apt/sources.list.d/cloudpassage.list > /dev/null"
 	when "redhat", "centos", "fedora"
 		#yum
-		command = "echo '[cloudpassage]\nname=CloudPassage production\nbaseurl=http://packages.cloudpassage.com/#{node[:cloudpassage][:repository_key]}/redhat/$basearch\ngpgcheck=1' | sudo tee /etc/yum.repos.d/cloudpassage.repo > /dev/null"
+		command = "echo '[cloudpassage]\nname=CloudPassage production\nbaseurl=http://packages.cloudpassage.com/#{node[:cloudpassage][:repository_key]}/redhat/$basearch\ngpgcheck=1' | tee /etc/yum.repos.d/cloudpassage.repo > /dev/null"
 end
 
 execute "cloudpassage-add-repository" do
@@ -28,11 +28,11 @@ case node[:platform]
 			action :install
 		end
 
-		command = "curl http://packages.cloudpassage.com/cloudpassage.packages.key | sudo apt-key add -"
-		gpg_key_already_installed = "sudo apt-key list | grep cloudpassage"
+		command = "curl http://packages.cloudpassage.com/cloudpassage.packages.key | apt-key add -"
+		gpg_key_already_installed = " apt-key list | grep cloudpassage"
 	when "redhat", "centos", "fedora"
-		command = "sudo rpm --import http://packages.cloudpassage.com/cloudpassage.packages.key"
-		gpg_key_already_installed = "sudo rpm -qa gpg-pubkey* | xargs -i rpm -qi {} | grep cloudpassage"
+		command = "rpm --import http://packages.cloudpassage.com/cloudpassage.packages.key"
+		gpg_key_already_installed = "rpm -qa gpg-pubkey* | xargs -i rpm -qi {} | grep cloudpassage"
 end
 
 execute "cloudpassage-import-public-key" do
@@ -44,7 +44,7 @@ end
 #update repositories
 case node[:platform]
 	when "debian", "ubuntu"
-		command = "sudo apt-get update"
+		command = "apt-get update"
 end
 
 execute "cloudpassage-update-repositories" do
@@ -60,7 +60,7 @@ end
 
 #start the daemon
 service "cphalod" do
-	start_command "sudo /etc/init.d/cphalod start --api-key=#{node[:cloudpassage][:license_key]}"
+	start_command "/etc/init.d/cphalod start --api-key=#{node[:cloudpassage][:license_key]}"
 	stop_command "service cphalod stop"
 	status_command "service cphalod status"
 	restart_command "service cphalod restart"
